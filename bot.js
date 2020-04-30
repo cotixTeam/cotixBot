47b07789c0dde77955f9a39e49b0332cc3716782
MMRREE <eamonntrim@gmail.com>
2020-04-30 17:47:19 +0200

    Began modularising leaderboards

--- bot.js
+++ bot.js
@@ -1,11 +1,29 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./local/auth.json');
+
+const Channels = { 
+    "Leaderboards": {
+        "id": "691318868475510834",
+        "messages": [{
+            "name": "CCS",
+            "id": "691591460176527361",
+            "default": "Carcassonne! Wins (Ranked) [!win CCS (@opponent1) (@opponent2)]\n<@!161848310578151424> - 0/0\n<@!264106235681308673> - 0/0\n<@!381175528847310851> - 0/0\n<@!140452063111806976> - 0/0\n"
+        },
+        {
+            "name": "UCH", 
+            "id": "691591631329296434", 
+            "default": "Ultimate Chicken Horse (Ranked) [!win UCH command to add win]\n<@!161848310578151424> - 0/0\n<@!264106235681308673> - 0/0\n<@!381175528847310851> - 0/0\n<@!140452063111806976> - 0/0\n" 
+        }]
+    }
+}
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
+
 logger.level = 'debug';
 
 // Initialize Discord Bot
@@ -13,6 +31,7 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+
 
 bot.on('ready', function (evt) {
     // Launch functionality, will update when need new rank systems
@@ -22,186 +41,120 @@ bot.on('ready', function (evt) {
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    if(channelID == '691318868475510834'){
+    if(channelID == Channels.Leaderboards.id){
         if (message.substring(0, 1) == '!') {
             var args = message.substring(1).split(' ');
             var cmd = args[0];
            
             args = args.splice(1);
+
             switch(cmd) {
-                case 'resetCCS':
-                    bot.editMessage({
-                        channelID: '691318868475510834',
-                        messageID: '691591460176527361',
-                        message: 'Carcassonne! Wins (Ranked) [!winCCS (@opponent1) (@opponent2)]\n<@!161848310578151424> - 0/0\n<@!264106235681308673> - 0/0\n<@!381175528847310851> - 0/0\n<@!140452063111806976> - 0/0'
-                    });
+                case 'reset':
+                    for(i = 0; i < Object.keys(Channels.Leaderboards.messages).length; i++){
+                        if(args[0] == Channels.Leaderboards.messages[i].name){
+                            bot.editMessage({
+                                channelID: Channels.Leaderboards.id,
+                                messageID: Channels.Leaderboards.messages[i].id,
+                                message: Channels.Leaderboards.messages[i].default
+                            });
+                            logger.info("Reset message " + Channels.Leaderboards.messages[i].id + " to " + Channels.Leaderboards.messages[i].default);
+                        }
+                    }
                 break;
                 
-                case 'winCCS':
-                   bot.getMessage({
-                    channelID: '691318868475510834',
-                    messageID: '691591460176527361'
-                }, (error, message)=>{
-                    var lines = message.content.split('\n');
-                    lines = lines.splice(1);
-                    var titleString = 'Carcassonne! Wins (Ranked) [!winCCS (@opponent1) (@opponent2)] \n';
-                    var workingStrings = [];
-
-                    lines.forEach((line, index)=>{
-                        if(userID == line.substr(3, 18)){
-                            line = line.substr(0, 25) + (parseInt(line.substr(25,1))+1) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
-                            workingStrings[index] = line;
-                        } else{
-                            var tempString = line + " \n";
-                            workingStrings[index] = tempString;
-                        }
-                        
-                    });
-
-                    args.forEach((arg, indexArg)=>{
-                        lines.forEach((line, indexLine)=>{
-                            if(arg.substr(3,18) == line.substr(3,18)){
-                                line = line.substr(0, 27) + (parseInt(line.substr(27,1))+1) + " \n";
-
-                                workingStrings[indexLine] = line;
-                            }
-                        });
-                    });
-
-                    workingString = titleString.concat(workingStrings.join(''));
-
-                    bot.editMessage({
-                        channelID: '691318868475510834',
-                        messageID: '691591460176527361',
-                        message: workingString
-                    });
-                });
-                break;
-
-                case 'winOtherCCS':
-                    bot.getMessage({
-                        channelID: '691318868475510834',
-                        messageID: '691591460176527361'
-                    }, (error, message)=>{
-                        var lines = message.content.split('\n');
-                        lines = lines.splice(1);
-                        var titleString = 'Carcassonne! Wins (Ranked) [!winCCS (@opponent1) (@opponent2)] \n';
-                        var workingStrings = [];
-
-                        var indexAdded = 0;
-
-                        args.forEach((arg, indexArg)=>{
-                            lines.forEach((line, indexLine)=>{
-                                if(arg.substr(3,18) == line.substr(3,18)){
-                                    if(indexAdded == 0){
-                                        indexAdded++;
-                                        line = line.substr(0, 25) + (parseInt(line.substr(25,1))+1) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
-                                    } else{
-                                        line = line.substr(0, 25) + (parseInt(line.substr(25,1))) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
-                                    }
-                                    workingStrings[indexLine] = line;
-                                } else if (indexArg == 0) {
+                case 'win':
+                for(i = 0; i < Object.keys(Channels.Leaderboards.messages).length; i++){
+                    if(args[0] == Channels.Leaderboards.messages[i].name){
+                        bot.getMessage({
+                            channelID: Channels.Leaderboards.id,
+                            messageID: Channels.Leaderboards.messages[i].id
+                        }, (error, message)=>{
+                            var lines = message.content.split('\n');
+                            var titleString = lines[0] + '\n';
+                            lines = lines.splice(1);
+                            var workingStrings = [];
+        
+                            lines.forEach((line, index)=>{
+                                if(userID == line.substr(3, 18)){
+                                    line = line.substr(0, 25) + (parseInt(line.substr(25,1))+1) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
+                                    workingStrings[index] = line;
+                                } else{
                                     var tempString = line + " \n";
-                                    workingStrings[indexLine] = tempString;
+                                    workingStrings[index] = tempString;
+                                }
+                                
+                            });
+        
+                            args.forEach((arg, indexArg)=>{
+                                if(indexArg != 0){
+                                    lines.forEach((line, indexLine)=>{
+                                        if(arg.substr(3,18) == line.substr(3,18)){
+                                            line = line.substr(0, 27) + (parseInt(line.substr(27,1))+1) + " \n";
+                                            workingStrings[indexLine] = line;
+                                        }
+                                    });
                                 }
                             });
-                        });
-
-                        workingString = titleString.concat(workingStrings.join(''));
-
-                        bot.editMessage({
-                            channelID: '691318868475510834',
-                            messageID: '691591460176527361',
-                            message: workingString
-                        });
-                    });
-                break;
-
-                case 'resetUCH':
-                    bot.editMessage({
-                        channelID: '691318868475510834',
-                        messageID: '691591631329296434',
-                        message: '"Ultimate Chicken Horse (Ranked) [!winUCH command to add win]\n<@!161848310578151424> - 0/0\n<@!264106235681308673> - 0/0\n<@!381175528847310851> - 0/0\n<@!140452063111806976> - 0/0'
-                    })
-                break;
-
-                case 'winUCH':
-                    bot.getMessage({
-                        channelID: '691318868475510834',
-                        messageID: '691591631329296434'
-                    }, (error, message)=>{
-                        var lines = message.content.split('\n');
-                        lines = lines.splice(1);
-                        var titleString = "Ultimate Chicken Horse (Ranked) [!winUCH (@opponent1) (@opponent2)] \n";
-                        var workingStrings = [];
-
-                        lines.forEach((line, index)=>{
-                            if(userID == line.substr(3, 18)){
-                                line = line.substr(0, 25) + (parseInt(line.substr(25,1))+1) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
-                                workingStrings[index] = line;
-                            } else{
-                                var tempString = line + " \n";
-                                workingStrings[index] = tempString;
-                            }
-                        });
-
-                        args.forEach((arg, indexArg)=>{
-                            lines.forEach((line, indexLine)=>{
-                                if(arg.substr(3,18) == line.substr(3,18)){
-                                    line = line.substr(0, 27) + (parseInt(line.substr(27,1))+1) + " \n";
-
-                                    workingStrings[indexLine] = line;
-                                }
+        
+                            workingString = titleString.concat(workingStrings.join(''));
+        
+                            bot.editMessage({
+                                channelID: Channels.Leaderboards.id,
+                                messageID: message.id,
+                                message: workingString
                             });
-                        });
 
-                        workingString = titleString.concat(workingStrings.join(''));
-
-                        bot.editMessage({
-                            channelID: '691318868475510834',
-                            messageID: '691591631329296434',
-                            message: workingString
+                            logger.info("Updated message " + message.id + " to " + workingString);
                         });
-                    });
+                    }
+                }
                 break;
 
-                case 'winOtherUCH':
-                    bot.getMessage({
-                        channelID: '691318868475510834',
-                        messageID: '691591631329296434'
-                    }, (error, message)=>{
-                        var lines = message.content.split('\n');
-                        lines = lines.splice(1);
-                        var titleString = "Ultimate Chicken Horse (Ranked) [!winUCH (@opponent1) (@opponent2)] \n";
-                        var workingStrings = [];
-
-                        var indexAdded = 0;
-
-                        args.forEach((arg, indexArg)=>{
-                            lines.forEach((line, indexLine)=>{
-                                if(arg.substr(3,18) == line.substr(3,18)){
-                                    if(indexAdded == 0){
-                                        indexAdded++;
-                                        line = line.substr(0, 25) + (parseInt(line.substr(25,1))+1) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
-                                    } else{
-                                        line = line.substr(0, 25) + (parseInt(line.substr(25,1))) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
+                case 'winOther':
+                for(i = 0; i < Object.keys(Channels.Leaderboards.messages).length; i++){
+                    if(args[0] == Channels.Leaderboards.messages[i].name){
+                        bot.getMessage({
+                            channelID: Channels.Leaderboards.id,
+                            messageID: Channels.Leaderboards.messages[i].id
+                        }, (error, message)=>{
+                            var lines = message.content.split('\n');
+                            var titleString = lines[0] + '\n';
+                            lines = lines.splice(1);
+                            var workingStrings = [];
+    
+                            var indexAdded = 0;
+    
+                            args.forEach((arg, indexArg)=>{
+                                lines.forEach((line, indexLine)=>{
+                                    if(indexArg != 0){
+                                        if(arg.substr(3,18) == line.substr(3,18)){
+                                            if(indexAdded == 0){
+                                                indexAdded++;
+                                                line = line.substr(0, 25) + (parseInt(line.substr(25,1))+1) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
+                                            } else{
+                                                line = line.substr(0, 25) + (parseInt(line.substr(25,1))) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
+                                            }
+                                            workingStrings[indexLine] = line;
+                                        } else if (indexArg == 0) {
+                                            var tempString = line + " \n";
+                                            workingStrings[indexLine] = tempString;
+                                        }
                                     }
-                                    workingStrings[indexLine] = line;
-                                } else if (indexArg == 0) {
-                                    var tempString = line + " \n";
-                                    workingStrings[indexLine] = tempString;
-                                }
+                                });
                             });
-                        });
+    
+                            workingString = titleString.concat(workingStrings.join(''));
+    
+                            bot.editMessage({
+                                channelID: Channels.Leaderboards.id,
+                                messageID: message.id,
+                                message: workingString
+                            });
 
-                        workingString = titleString.concat(workingStrings.join(''));
-
-                        bot.editMessage({
-                            channelID: '691318868475510834',
-                            messageID: '691591631329296434',
-                            message: workingString
+                            logger.info("Updated message " + message.id + " to " + workingString);
                         });
-                    });
+                    }
+                }
                 break;
              }
          }
