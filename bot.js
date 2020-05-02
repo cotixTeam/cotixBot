72523f9d78aea2787e6c7352f2ca4e3a26335cc0
Eamonn Trim <ect1u17@soton.ac.uk>
2020-05-02 21:39:05 +0200

    began porting to discord js - unsure if working

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./local/auth.json');
+const Discord = require('discord.js');
+const logger = require('winston');
+const auth = require('./local/auth.json');
+const bot = new Discord.Client();
+
+bot.login(auth.token);
 
 const Channels = { 
     "Leaderboards": {
@@ -18,7 +21,6 @@ const Channels = {
     }
 }
 
-// Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
@@ -26,26 +28,22 @@ logger.add(new logger.transports.Console, {
 
 logger.level = 'debug';
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-
-
-bot.on('ready', function (evt) {
-    // Launch functionality, will update when need new rank systems
+bot.on('ready', () => {
     logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Logged in as: ' + bot.user.username + ' (' + bot.user.id + ')');
 });
 
-bot.on('message', function (user, userID, channelID, messageReceived, evt) {
-    if(channelID == Channels.Leaderboards.id){
-        if (messageReceived.substring(0, 1) == '!') {
-            let args = messageReceived.substring(1).split(' ');
+bot.on('message', (messageReceived) => {
+    let receivedContent = messageReceived.content;
+    let channelID = messageReceived.channel;
+    let guildID = messageReceived.guild;
+    let receivedUserId = messageReceived.userID;
+    
+        if(channelID == Channels.Leaderboards.id){
+            let leaderboardChannel = new Discord.TextChannel(guildID, {id: Channels.Leaderboards.id});
+        if (receivedContent.substring(0, 1) == '!') {
+            let args = receivedContent.substring(1).split(' ');
             let cmd = args[0];
-            let messageReceivedId = evt.d.id;
            
             args = args.splice(1);
 
@@ -53,19 +51,11 @@ bot.on('message', function (user, userID, channelID, messageReceived, evt) {
                 case 'reset':
                     for(let game of Channels.Leaderboards.games){
                         if(args[0] == game.name){
-                            bot.editMessage({
-                                channelID: Channels.Leaderboards.id,
-                                messageID: game.messageId,
-                                message: game.defaultMessage
-                            }, (error, response)=>{
-                                bot.deleteMessage({
-                                    channelID: Channels.Leaderboards.id,
-                                    messageID: messageReceivedId
-                                }, (error, response)=>{
-                                    if(error == null) logger.info("Succesfully delete the message");
+                            new Discord.Message(bot, {id: game.messageId}, leaderboardChannel).edit(game.defaultMessage).then(()=>{
+                                messageReceived.delete().then((e)=>{
+                                    logger.error(e);
                                 });
-                            });
-                                
+                            });                                
                             logger.info("Reset message " + game.messageId + " to " + game.defaultMessage);
                         }
                     }
@@ -74,54 +64,44 @@ bot.on('message', function (user, userID, channelID, messageReceived, evt) {
                 case 'win':
                 for(let game of Channels.Leaderboards.games){
                     if(args[0] == game.name){
-                        bot.getMessage({
-                            channelID: Channels.Leaderboards.id,
-                            messageID: game.messageId
-                        }, (error, editMessage)=>{
-                            let lines = editMessage.content.split('\n');
-                            let titleString = lines[0] + '\n';
-                            lines = lines.splice(1);
-                            let workingStrings = [];
-        
-                            lines.forEach((line, index)=>{
-                                if(userID == line.substr(3, 18)){
-                                    line = line.substr(0, 25) + (parseInt(line.substr(25,1))+1) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
-                                    workingStrings[index] = line;
-                                } else{
-                                    let tempString = line + " \n";
-                                    workingStrings[index] = tempString;
-                                }
-                                
-                            });
-        
-                            args.forEach((arg, indexArg)=>{
-                                if(indexArg != 0){
-                                    lines.forEach((line, indexLine)=>{
-                                        if(arg.substr(3,18) == line.substr(3,18)){
-                                            line = line.substr(0, 27) + (parseInt(line.substr(27,1))+1) + " \n";
-                                            workingStrings[indexLine] = line;
-                                        }
-                                    });
-                                }
-                            });
-        
-                            workingString = titleString.concat(workingStrings.join(''));
-        
-                            bot.editMessage({
-                                channelID: Channels.Leaderboards.id,
-                                messageID: editMessage.id,
-                                message: workingString
-                            }).then(()=>{
-                                bot.deleteMessage({
-                                    channelID: Channels.Leaderboards.id,
-                                    messageID: messageReceivedId
-                                }, (error, response)=>{
-                                    if(error == null) logger.info("Succesfully delete the message");
-                                });
-                            });
 
-                            logger.info("Updated message " + editMessage.id + " to " + workingString);
+                        let editMessage = new Discord.Message(bot, {id: game.messageId}, leaderboardChannel);
+                        let lines = editMessage.content.split('\n');
+                        let titleString = lines[0] + '\n';
+                        lines = lines.splice(1);
+                        let workingStrings = [];
+    
+                        lines.forEach((line, index)=>{
+                            if(userID == line.substr(3, 18)){
+                                line = line.substr(0, 25) + (parseInt(line.substr(25,1))+1) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
+                                workingStrings[index] = line;
+                            } else{
+                                let tempString = line + " \n";
+                                workingStrings[index] = tempString;
+                            }
+                            
                         });
+    
+                        args.forEach((arg, indexArg)=>{
+                            if(indexArg != 0){
+                                lines.forEach((line, indexLine)=>{
+                                    if(arg.substr(3,18) == line.substr(3,18)){
+                                        line = line.substr(0, 27) + (parseInt(line.substr(27,1))+1) + " \n";
+                                        workingStrings[indexLine] = line;
+                                    }
+                                });
+                            }
+                        });
+    
+                        workingString = titleString.concat(workingStrings.join(''));
+
+                        editMessage.edit(workingString).then(()=>{
+                            messageReceived.delete((e)=>{
+                                logger.error(e);
+                            })
+                        });
+
+                        logger.info("Updated message " + editMessage.id + " to " + workingString);
                     }
                 }
                 break;
@@ -129,71 +109,51 @@ bot.on('message', function (user, userID, channelID, messageReceived, evt) {
                 case 'winOther':
                 for(let game of Channels.Leaderboards.games){
                     if(args[0] == game.name){
-                        bot.getMessage({
-                            channelID: Channels.Leaderboards.id,
-                            messageID: game.messageId
-                        }, (error, editMessage)=>{
-                            let lines = editMessage.content.split('\n');
-                            let titleString = lines[0] + '\n';
-                            lines = lines.splice(1);
-                            let workingStrings = [];
-    
-                            let indexAdded = 0;
-    
-                            args.forEach((arg, indexArg)=>{
-                                lines.forEach((line, indexLine)=>{
-                                    if(indexArg != 0){
-                                        if(arg.substr(3,18) == line.substr(3,18)){
-                                            if(indexAdded == 0){
-                                                indexAdded++;
-                                                line = line.substr(0, 25) + (parseInt(line.substr(25,1))+1) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
-                                            } else{
-                                                line = line.substr(0, 25) + (parseInt(line.substr(25,1))) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
-                                            }
-                                            workingStrings[indexLine] = line;
-                                        } else if (indexArg == 0) {
-                                            let tempString = line + " \n";
-                                            workingStrings[indexLine] = tempString;
-                                        }
-                                    }
-                                });
-                            });
-    
-                            workingString = titleString.concat(workingStrings.join(''));
-    
-                            bot.editMessage({
-                                channelID: Channels.Leaderboards.id,
-                                messageID: editMessage.id,
-                                message: workingString
-                            });
 
-                            logger.info("Updated message " + editMessage.id + " to " + workingString);
-                        }).then(()=>{
-                            bot.deleteMessage({
-                                channelID: Channels.Leaderboards.id,
-                                messageID: messageReceivedId
-                            }, (error, response)=>{
-                                if(error == null) logger.info("Succesfully delete the message");
-                            })
+                        let editMessage = new Discord.Message(bot, {id: game.messageId}, leaderboardChannel);
+                        let lines = editMessage.content.split('\n');
+                        let titleString = lines[0] + '\n';
+                        lines = lines.splice(1);
+                        let workingStrings = [];    
+                        let indexAdded = 0;
+
+                        args.forEach((arg, indexArg)=>{
+                            lines.forEach((line, indexLine)=>{
+                                if(indexArg != 0){
+                                    if(arg.substr(3,18) == line.substr(3,18)){
+                                        if(indexAdded == 0){
+                                            indexAdded++;
+                                            line = line.substr(0, 25) + (parseInt(line.substr(25,1))+1) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
+                                        } else{
+                                            line = line.substr(0, 25) + (parseInt(line.substr(25,1))) + "/" + (parseInt(line.substr(27,1))+1) + " \n";
+                                        }
+                                        workingStrings[indexLine] = line;
+                                    } else if (indexArg == 0) {
+                                        let tempString = line + " \n";
+                                        workingStrings[indexLine] = tempString;
+                                    }
+                                }
+                            });
                         });
+
+                        workingString = titleString.concat(workingStrings.join(''));
+
+                        editMessage.edit(workingString).then(()=>{
+                            messageReceived.delete((e)=>{
+                                logger.error(e);
+                            });
+                        });
+
+                        logger.info("Updated message " + editMessage.id + " to " + workingString);
                     }
                 }
                 break;
 
                 default:
-                bot.sendMessage({
-                    to: userID,
-                    message: "Hi " + user + ",\n'" + messageReceived + "' is not an implemented command!"
-                }, (error, response)=>{
-                    logger.info(response); 
-                    if(error == null){
-                        bot.deleteMessage({
-                            channelID: Channels.Leaderboards.id,
-                            messageID: messageReceivedId
-                        }, (error, response)=>{
-                            if(error == null) logger.info("Succesfully delete the message");
-                        })
-                    }
+                new Discord.User(bot, {id: messageReceived.userID}).send("Hi " + messageReceived.username + ",\n'" + cmd + "' is not an implemented command!").then(()=>{
+                    messageReceived.delete((e)=>{
+                        logger.error(e);
+                    });
                 });
                 break;
              }
