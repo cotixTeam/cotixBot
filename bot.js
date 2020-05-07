627f4a5260a6369248290d40238db5d495587331
MMRREE <eamonntrim@gmail.com>
2020-05-07 18:28:11 +0200

    added reminders and changed buildspec and appspec to take every file

--- bot.js
+++ bot.js
@@ -7,19 +7,14 @@ const IdeasClass = require('./bot/ideas.js');
 const LeaderboardClass = require('./bot/leaderboard.js');
 const ReminderClass = require('./bot/reminder.js');
 
-// Parsed JSON files & prevent fatal crashes with catches
-let Reminders = null
-try {
-    Reminders = JSON.parse(FileSystem.readFileSync("./bot/config/Reminders.json"));
-} catch (err) {
-    console.error(err);
-}
 
+// Parsed JSON files & prevent fatal crashes with catches
 let Channels = null
 try {
     Channels = JSON.parse(FileSystem.readFileSync("./bot/config/Channels.json"));
 } catch (err) {
     console.error(err);
+    process.exit();
 }
 
 // Object creation
@@ -46,45 +41,12 @@ bot.on('ready', () => { // Run init code
         },
         status: "online"
     });
-
-    let reminderDate = new Date();
-    reminderDate.setSeconds(0);
-    reminderDate.setMilliseconds(0);
-
-    for (let reminder of Reminders) {
-        reminderDate.setDate(reminderDate.getDate() + reminder.day - reminderDate.getDay());
-        reminderDate.setHours(reminder.hour);
-        reminderDate.setMinutes(reminder.minute);
-        if (reminderDate.getTime() - (new Date()).getTime() >= 0) // If later today or this week
-            setTimeout(timeoutReminderFunction, reminderDate.getTime() - (new Date()).getTime(), reminder, reminderDate);
-        else // If any time before this time next week, set for next week
-            setTimeout(timeoutReminderFunction, reminderDate.getTime() - (new Date()).getTime() + 7 * 24 * 60 * 60 * 1000, reminder, reminderDate);
-    }
 });
-
-function timeoutReminderFunction(reminder, reminderDate) {
-    for (let user of reminder.users) {
-        bot.users.fetch(user)
-            .then((userSend) => {
-                userSend.send("Hi " + userSend.username + ",\n This is your reminder for: '" + reminder.name + "'\n" + reminder.text);
-            }).then(() => {
-                setTimeout(timeoutReminderFunction.bind(user, reminder, reminderDate), reminderDate.getTime() + 7 * 24 * 60 * 60 * 1000 - (new Date()).getTime()); // Once started once, the reminder will go weekly
-            });
-    }
-}
 
 function notImplementedCommand(messageReceived, cmd) {
     messageReceived.author
         .send("Hi " + messageReceived.author.username + ",\n'" + cmd + "' is not an implemented command!")
         .then((sentMessage) => {
-            messageReceived.delete();
-        });
-}
-
-function sendPlaceholderMessage(messageReceived) {
-    messageReceived.channel
-        .send('Placeholder Message')
-        .then(() => {
             messageReceived.delete();
         });
 }
@@ -102,24 +64,28 @@ bot.on('message', (messageReceived) => {
 
         if (messageContent == "!sendPlaceholder") {
             console.log("Sending placeholder!");
-            sendPlaceholderMessage(messageReceived);
+            messageReceived.channel
+                .send('Placeholder Message')
+                .then(() => {
+                    messageReceived.delete();
+                });
         } else {
             switch (messageReceived.channel.id) {
                 case Channels.Settings.id:
                     switch (cmd) {
                         case 'listEvents':
                             console.log("Listing events that can be added to reminder!");
-                            reminder.listEvents(messageReceived, Reminders);
+                            reminder.listEvents(messageReceived);
                             break;
 
                         case 'joinReminder':
                             console.log("Joining notification list for event!");
-                            reminder.joinReminder(messageReceived, argumentString, Reminders);
+                            reminder.joinReminder(messageReceived, argumentString);
                             break;
 
                         case 'leaveReminder':
                             console.log("Leaving notification list for event!");
-                            reminder.leaveReminder(messageReceived, argumentString, Reminders);
+                            reminder.leaveReminder(messageReceived, argumentString);
                             break;
 
                         default:
--- bot/reminder.js
+++ bot/reminder.js
@@ -7,12 +7,46 @@ class ReminderClass {
     constructor(client, channels) {
         this.bot = client;
         this.channels = channels;
+
+        this.Reminders = null
+        try {
+            this.Reminders = JSON.parse(FileSystem.readFileSync("./bot/config/Reminders.json"));
+        } catch (err) {
+            console.error(err);
+            this.bot.destroy();
+            process.exit();
+        }
+
+        let reminderDate = new Date();
+        reminderDate.setSeconds(0);
+        reminderDate.setMilliseconds(0);
+
+        for (let reminder of this.Reminders) {
+            reminderDate.setDate(reminderDate.getDate() + reminder.day - reminderDate.getDay());
+            reminderDate.setHours(reminder.hour);
+            reminderDate.setMinutes(reminder.minute);
+            if (reminderDate.getTime() - (new Date()).getTime() >= 0) // If later today or this week
+                setTimeout(this.timeoutReminderFunction, reminderDate.getTime() - (new Date()).getTime(), reminder, reminderDate);
+            else // If any time before this time next week, set for next week
+                setTimeout(this.timeoutReminderFunction, reminderDate.getTime() - (new Date()).getTime() + 7 * 24 * 60 * 60 * 1000, reminder, reminderDate);
+        }
     }
 
-    listEvents(messageReceived, Reminders) {
+    timeoutReminderFunction(reminder, reminderDate) {
+        for (let user of reminder.users) {
+            bot.users.fetch(user)
+                .then((userSend) => {
+                    userSend.send("Hi " + userSend.username + ",\n This is your reminder for: '" + reminder.name + "'\n" + reminder.text);
+                }).then(() => {
+                    setTimeout(this.timeoutReminderFunction.bind(user, reminder, reminderDate), reminderDate.getTime() + 7 * 24 * 60 * 60 * 1000 - (new Date()).getTime()); // Once started once, the reminder will go weekly
+                });
+        }
+    }
+
+    listEvents(messageReceived) {
         let workingStrings = []
         let index = 0;
-        for (let reminder of Reminders) {
+        for (let reminder of this.Reminders) {
             workingStrings[index++] = reminder.name;
         }
 
@@ -25,8 +59,8 @@ class ReminderClass {
             });
     }
 
-    joinReminder(messageReceived, argumentString, Reminders) {
-        for (let reminder of Reminders) {
+    joinReminder(messageReceived, argumentString) {
+        for (let reminder of this.Reminders) {
             if (reminder.name == argumentString) {
                 // Add the thing to the file
                 let addFlag = true;
@@ -36,8 +70,8 @@ class ReminderClass {
                 }
 
                 if (addFlag) {
-                    Reminders[Reminders.indexOf(reminder)].users.push(messageReceived.author.id)
-                    FileSystem.writeFile("./bot/config/Reminders.json", JSON.stringify(Reminders, null, '\t'), this.catchError);
+                    this.Reminders[this.Reminders.indexOf(reminder)].users.push(messageReceived.author.id)
+                    FileSystem.writeFile("./bot/config/this.Reminders.json", JSON.stringify(this.Reminders, null, '\t'), this.catchError);
                     messageReceived.author.send("You have been added to the reminder: " + reminder.name);
                 } else {
                     messageReceived.author.send("You are already registerd to the reminder: " + reminder.name);
@@ -47,8 +81,8 @@ class ReminderClass {
         messageReceived.delete();
     }
 
-    leaveReminder(messageReceived, argumentString, Reminders) {
-        for (let reminder of Reminders) {
+    leaveReminder(messageReceived, argumentString) {
+        for (let reminder of this.Reminders) {
             if (reminder.name == argumentString) {
                 // Remove the thing fromm the file
                 let userExists = false;
@@ -61,8 +95,8 @@ class ReminderClass {
                 }
 
                 if (userExists) {
-                    Reminders[Reminders.indexOf(reminder)].users.splice(userIndex, 1);
-                    FileSystem.writeFile("./bot/config/Reminders.json", JSON.stringify(Reminders, null, '\t'), this.catchError);
+                    this.Reminders[this.Reminders.indexOf(reminder)].users.splice(userIndex, 1);
+                    FileSystem.writeFile("./bot/config/this.Reminders.json", JSON.stringify(this.Reminders, null, '\t'), this.catchError);
                     messageReceived.author.send("You have been removed from the reminder: " + reminder.name);
                 } else {
                     messageReceived.author.send("You are not registerd to the reminder: " + reminder.name);
