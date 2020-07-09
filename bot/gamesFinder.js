3dbfbd526665812895d19cf3b750f87a8f3bc121
Eamonn Trim <ect1u17@soton.ac.uk>
2020-07-09 22:23:28 +0200

    removed steamapi dependancy and added comments for how we are going to work around steam api being weird

--- bot/gamesFinder.js
+++ bot/gamesFinder.js
@@ -1,125 +1,101 @@
-/// SETUP ///
-
-const SteamAPI = require('steamapi');
-const steam = new SteamAPI("DD5D126C29EED3972000CE74F8BFBAA4");
+const rp = require('request-promise-native');
 var me = "76561198037408655";
-
-
-/// INTERMEDIATE FUNCTIONS ///
-
+const steamKey = "2C16D2888B4E4709FCDAB4D8A33B92EE";
 var friendsUsernames = ['Gavin8a2can', 'MisterE', 'thialfi'];
 
-/// Fill an array friendsUsernames with their IDs
+// This all works as intended, but steam api randomly does not provide the results for games libraries.
+// This means a work around has to be made so that when the results are missing, a cached version is used, and then that can be compared
+// This should not be too difficult to do, but when an empty result is found, and a cache cannot replace it, the command should abort,
+// Notifying the users that steam api is being a bitch, otherwise continue by using the cached / updated lists
+// The cache will not be backed up anything (such as s3) so will be lost between restarts, but this does not seem like that big of an issue
+
 async function fillFriendsIDList(refSteamID, friendsUsernames) {
     let foundFriends = [];
-    //get List of Friend Objects
-    let friends = await steam.getUserFriends(refSteamID);
-    for (unknownFriend of friends) {
-        let summary = await steam.getUserSummary(unknownFriend["steamID"]);
-        for (knownFriendName of friendsUsernames) {
-            if (summary["nickname"] == knownFriendName) {
-                //console.log("Found 1...");
-                foundFriends.push(summary["steamID"]);
+
+    let friends = await rp.get("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=" + steamKey +
+        "&relationship=friend" +
+        "&steamid=" + refSteamID).then((res) => {
+        let jsonRes = JSON.parse(res);
+        console.log(jsonRes.friendslist.friends)
+        return jsonRes.friendslist.friends;
+    });
+
+    friends = friends.map((friend) => {
+        return friend.steamid
+    });
+
+    let steamFriends = await rp.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + steamKey +
+        "&steamids=" + friends.join(",")).then((res) => {
+        let jsonRes = JSON.parse(res);
+        console.log(jsonRes.response.players);
+        return jsonRes.response.players
+    });
+
+    for (let unknownFriend of steamFriends) {
+        for (let knownFriendName of friendsUsernames) {
+            if (unknownFriend.personaname == knownFriendName) {
+                console.log("Adding " + unknownFriend.personaname + " to the list with id " + unknownFriend.steamid);
+                foundFriends.push(unknownFriend.steamid);
             }
         }
     }
     return foundFriends;
 };
 
-/// Fill an array with multiple lists of Game Objects --> [gList1, gList2, gList3] where each gList = [Game1, Game2, Game3]
-
-
 async function getGameList(refID, idList) {
-    //Add my game id to everyone else's
     idList.push(refID);
     let gameList = [];
-    //iterate through the list and retrive a game list
-    for (id of idList) {
-        let gList = await steam.getUserOwnedGames(id);
+
+    for (let id of idList) {
+        console.log(id)
+        let gList = await rp.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + steamKey +
+            "&include_appinfo=1" +
+            "&include_played_free_games=1" +
+            "&steamid=" + id +
+            "&format=json").then((res) => {
+            let jsonRes = JSON.parse(res);
+            console.log(jsonRes.response.games);
+            return jsonRes.response.games;
+        })
         gameList.push(gList);
-        //console.log(id + ": " + gList.length + " games found!")
     }
-    return gameList
+    return gameList;
 }
 
-// Iterate through list of games lists and find shortest one, place that in shortestList
-async function getShortest(fullGameList) {
-    let min;
-    let minI;
-    for (gList in fullGameList) {
-        let x = fullGameList[gList].length;
-        if (min == null) {
-            min = x;
-            minI = gList;
-        } else if (x < min) {
-            min = x;
-            minI = gList;
-        }
-    }
-    //console.log(min + ": " + minI);
-    let shortGameList = fullGameList[minI];
-    //console.log(shortGameList);
-    fullGameList.splice(minI, 1);
-    return shortGameList;
-
-}
-
-async function getMatching(gameListOne, gameListTwo) {
+function getMatching(gameListOne, gameListTwo) {
     let matchingGames = [];
-    gameListOne.forEach(game => {
-        for (game2 of gameListTwo) {
-            if (game["name"] == game2["name"]) {
-                matchingGames.push(game);
-            }
+    for (let game of gameListOne) {
+        if (gameListTwo.some((game2) => game2.name == game.name)) {
+            matchingGames.push(game);
         }
-    });
+    }
     return matchingGames;
 }
 
-async function chooseGame(gameList) {
-    let randomGame = Math.round(Math.random() * gameList.length);
-    return gameList[randomGame]["name"];
+function chooseGame(gameList) {
+    if (gameList != null && gameList.length != 0) {
+        let randomGame = Math.round(Math.random() * gameList.length);
+        return gameList[randomGame]["name"];
+    } else return null;
 }
 
-
-/// MAIN USE FUNCTION ///
-/// getRandomGame((strGame, arrayofGameObjects) => {
-///     msg.reply(strGame);
-///     for(i in arrayofGameObjects) {
-///         msg.reply(lists[i]["name"]);
-///         //prints out all games
-///        }
-/// })
-
 async function getRandomGame() {
-    // fillFriendsIDList(refSteamID , friendsUsernames)
-    // getGameList(refID, idList, callback)
-
-    //Get friendsIDs
-    //console.log("Getting friends IDs...");
     var friendsIDs = await fillFriendsIDList(me, friendsUsernames);
-    //console.log(friendsIDs);
+
     var gameLists = await getGameList(me, friendsIDs, (val) => {
         return val
     });
-    let shortest = await getShortest(gameLists); //note shortest has now been removed from gameLists
-    let next_shortest = await getShortest(gameLists);
-    //console.log(gameLists.length);
-    let shortestUnion = await getMatching(shortest, next_shortest);
-    //console.log(shortestUnion);
-    //for remaining list members, see if they have any of the shortestUnion games
-    //union doeson't need to exist, but nice to separate shortestUnion and genericUnions
-    let union = shortestUnion;
-    for (remainder of gameLists) {
-        //getMatching takes two lists of Game objects
-        union = await getMatching(union, remainder);
+    gameLists = gameLists.filter((list) => list != null).sort((a, b) => a.length - b.length);
+
+    let combinedResults = gameLists[0];
+    gameLists = gameLists.splice(1);
+
+    for (let remainder of gameLists) {
+        combinedResults = getMatching(combinedResults, remainder);
     }
-    for (game of union) {
-        //console.log("We all of have: " + game["name"]);
-        // steam.getGameDetails(game["appID"]).then(val => { console.log(val["categories"])});
-    }
-    let chosenGame = await chooseGame(union);
-    return [chosenGame, union];
+
+    let chosenGame = chooseGame(combinedResults);
+    return [chosenGame, combinedResults];
 }
 
 exports.findGames = function (messageReceived) {
@@ -129,22 +105,26 @@ exports.findGames = function (messageReceived) {
         let list = results[1];
 
         let fields = [];
-        for (let game of list) {
-            fields.push({
-                name: "We could have played",
-                value: game.name,
-                inline: true
+        if (list != null && chosenGame != null) {
+            for (let game of list) {
+                fields.push({
+                    name: game.name,
+                    value: "... or ...",
+                    inline: true
+                })
+            }
+            message.edit({
+                "content": chosenGame,
+                "embed": {
+                    "title": "The game chosen was :    " + chosenGame,
+                    "description": "or you could have played...",
+                    "fields": fields
+                }
             })
+        } else {
+            message.edit("You have no games in common, sorry!");
         }
 
-        message.edit({
-            "content": chosenGame,
-            "embed": {
-                "title": "The game chose was:",
-                "description": chosenGame,
-                "fields": fields
-            }
-        })
     });
     messageReceived.delete();
 }
